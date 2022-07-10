b52fbaa5a1ced5707218edeabd92db0a697a0595
Tiger Wang <tiger.wangj@gmail.com>
2022-07-10 16:56:35 -0700

    Allow users to use gitignore templates in existing repositories

--- app/src/ui/repository-settings/git-ignore.tsx
+++ app/src/ui/repository-settings/git-ignore.tsx
@@ -3,6 +3,12 @@ import { DialogContent } from '../dialog'
 import { TextArea } from '../lib/text-area'
 import { LinkButton } from '../lib/link-button'
 import { Ref } from '../lib/ref'
+import { getGitIgnoreNames, getGitIgnoreText } from '../add-repository/gitignores'
+import { Button } from '../lib/button'
+import { IMenuItem, showContextualMenu } from '../../lib/menu-item'
+import { Octicon } from '../octicons/octicon'
+import * as OcticonSymbol from '../octicons/octicons.generated'
+import { Row } from '../lib/row'
 
 interface IGitIgnoreProps {
   readonly text: string | null
@@ -10,8 +16,41 @@ interface IGitIgnoreProps {
   readonly onShowExamples: () => void
 }
 
+interface IGitIgnoreState {
+  /** The names for the available gitignores. */
+  readonly gitIgnoreNames: ReadonlyArray<string> | null
+}
+
 /** A view for creating or modifying the repository's gitignore file */
-export class GitIgnore extends React.Component<IGitIgnoreProps, {}> {
+export class GitIgnore extends React.Component<IGitIgnoreProps, IGitIgnoreState> {
+  public constructor(props: IGitIgnoreProps) {
+    super(props)
+
+    this.state = {
+      gitIgnoreNames: null,
+    }
+  }
+
+  public async componentDidMount() {
+    const gitIgnoreNames = await getGitIgnoreNames()
+
+    this.setState({ gitIgnoreNames })
+  }
+
+  private onIgnoreTemplateSelected = async (templateName: string) => {
+    const templateText = await getGitIgnoreText(templateName)
+    this.props.onIgnoreTextChanged(templateText)
+  }
+
+  private onUseTemplateButtonClick = () => {
+    const gitIgnores = this.state.gitIgnoreNames || []
+    const items: IMenuItem[] = gitIgnores.map(n => ({
+      label: n,
+      action: () => this.onIgnoreTemplateSelected(n),
+    }))
+    showContextualMenu(items)
+  }
+
   public render() {
     return (
       <DialogContent>
@@ -23,6 +62,15 @@ export class GitIgnore extends React.Component<IGitIgnoreProps, {}> {
             Learn more
           </LinkButton>
         </p>
+
+        <Row>
+          <Button
+            onClick={this.onUseTemplateButtonClick}
+          >
+            Use Template
+            <Octicon symbol={OcticonSymbol.triangleDown} />
+          </Button>
+        </Row>
 
         <TextArea
           placeholder="Ignored files"
